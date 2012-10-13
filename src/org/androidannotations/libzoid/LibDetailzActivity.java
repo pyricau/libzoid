6161612392ab0a2d729b35d83aaf1119b71e2d40
Pierre-Yves Ricau <py.ricau@gmail.com>
2012-10-13 16:16:24 +0200

    last minute changes

--- src/org/androidannotations/libzoid/LibDetailzActivity.java
+++ src/org/androidannotations/libzoid/LibDetailzActivity.java
@@ -67,7 +67,6 @@ public class LibDetailzActivity extends Activity {
 		progressBar = (ProgressBar) findViewById(R.id.progressBar);
 		libImageView = (ImageView) findViewById(R.id.libImageView);
 
-		// You listening to me?
 		startProgressButton.setOnClickListener(new OnClickListener() {
 
 			@Override
